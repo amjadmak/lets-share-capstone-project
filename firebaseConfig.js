f2d841e05ab51d564aa0053104cc4a19fa85e6e2
Amjad-Mak <95312230+amjadmak@users.noreply.github.com>
2024-01-29 20:30:03 +0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -10,7 +10,7 @@ import {
     getStorage,
     uploadBytes,
 } from "firebase/storage";
-
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
